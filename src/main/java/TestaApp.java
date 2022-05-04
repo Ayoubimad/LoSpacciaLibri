7ecedcaa4398359d80ad77dde524b58c7ecce700
Ayoub Imad <82643685+Ayoubimad@users.noreply.github.com>
2022-05-04 20:39:06 +0200

    .

--- src/main/java/TestaApp.java
+++ src/main/java/TestaApp.java
@@ -1,3 +1,4 @@
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TestaApp {
@@ -9,10 +10,18 @@ public class TestaApp {
     public void run() throws SQLException {
         DBManager.openMySQLConnection();
         if(DBManager.connection != null && DBManager.statement!= null){
-            System.out.print("connessione avvenuta");
+            System.out.print("connessione avvenuta\n\n");
         }
 
+        DBManager.statement.execute("drop table if exists users;");
+        DBManager.statement.execute("create table users(\n" + "username varchar(25) PRIMARY KEY,\n" + "pw varchar(25)\n" + ");");
         DBManager.statement.execute("insert into users values('Ayoub','password')");
+        ResultSet rs = DBManager.statement.executeQuery("select * from users");
+        /*stampo la tabella, c'è solo Ayoub , passowrd*/
+        while(rs.next()){
+            System.out.println(String.format("username= %s , pw= %s", rs.getString("username"),rs.getString("pw")));
+        }
+
         DBManager.closeMySQLConnecion();
     }
 
