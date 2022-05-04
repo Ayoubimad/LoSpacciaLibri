e91027e64116f6d5ef82f0c67e621e669f77e8e2
Ayoub Imad <82643685+Ayoubimad@users.noreply.github.com>
2022-05-04 20:12:27 +0200

    prova DB

--- src/main/java/TestaApp.java
+++ src/main/java/TestaApp.java
@@ -1,8 +1,26 @@
+import java.sql.SQLException;
+
 public class TestaApp {
+
+    TestaApp() throws SQLException {
+       run();
+    }
+
+    public void run() throws SQLException {
+        DBManager.openMySQLConnection();
+        if(DBManager.connection != null && DBManager.statement!= null){
+            System.out.print("connessione avvenuta");
+        }
+
+        DBManager.statement.execute("insert into users values('Ayoub','password')");
+        DBManager.closeMySQLConnecion();
+    }
+
     public static void main(String[] args) {
-     System.out.print(true);
-     System.out.println("ciao");
-        System.out.print(true);
-        System.out.println("ciao");
+       try {
+           new TestaApp();
+       }catch (SQLException e){
+           e.printStackTrace();
+       }
     }
 }
