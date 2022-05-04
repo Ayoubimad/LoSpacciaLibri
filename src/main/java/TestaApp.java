eee00b28e00928e46b974ea76b595f059624f285
Ayoub Imad <82643685+Ayoubimad@users.noreply.github.com>
2022-05-04 19:12:30 +0200

    fff

--- src/main/java/TestaApp.java
+++ src/main/java/TestaApp.java
@@ -2,5 +2,7 @@ public class TestaApp {
     public static void main(String[] args) {
      System.out.print(true);
      System.out.println("ciao");
+        System.out.print(true);
+        System.out.println("ciao");
     }
 }
